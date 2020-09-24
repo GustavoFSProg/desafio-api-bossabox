f2392fef59da057f9fae33dd5c3ca6df04e85e45
Gustavo <gustavosohne38@gmail.com>
2020-09-24 19:14:30 -0300

    finished

--- src/controlers/toolsController.js
+++ src/controlers/toolsController.js
@@ -10,6 +10,16 @@ async function getAll(req, res) {
   }
 }
 
+async function getByTag(req, res) {
+  try {
+    const data = await toolsModel.findOne({ tags: req.params.tags })
+
+    return res.status(200).send({ data })
+  } catch (error) {
+    return res.status(200).send({ error })
+  }
+}
+
 async function create(req, res) {
   try {
     await toolsModel.create({
@@ -25,4 +35,14 @@ async function create(req, res) {
   }
 }
 
-export default { create, getAll }
+async function deleteById(req, res) {
+  try {
+    await toolsModel.findByIdAndRemove(req.params.id)
+
+    return res.status(204).send()
+  } catch (error) {
+    return res.status(400).send({ error })
+  }
+}
+
+export default { create, getAll, getByTag, deleteById }
--- src/routes/toolsRoute.js
+++ src/routes/toolsRoute.js
@@ -4,7 +4,8 @@ import toolsController from '../controlers/toolsController'
 const routes = new Router()
 const routeList = [
   routes.get('/', toolsController.getAll),
-
+  routes.delete('/tools/:id', toolsController.deleteById),
+  routes.get('/tools/:tags', toolsController.getByTag),
   routes.post('/tools', toolsController.create),
 ]
 

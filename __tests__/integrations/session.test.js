bb3fc11f984ede1ba46b70528e339e1dc3db26f6
Gustavo <gustavosohne38@gmail.com>
2020-09-30 18:27:25 -0300

    feito

--- __tests__/integrations/session.test.js
+++ __tests__/integrations/session.test.js
@@ -8,12 +8,16 @@ dotenv.config()
 describe('Create Tools', () => {
   it('should create tools model', async () => {
     const string = Math.random().toString(36).substring(2, 15)
-    const response = await request(app).post('/tools').send({
-      title: string,
-      description: 'Desc',
-      link: 'http://localhost',
-      // tags: ['owner'],
-    })
+    const response = await request(app)
+      .post('/tools')
+      .send({
+        title: string,
+        description: 'Desc',
+        link: 'http://localhost',
+        tags: ['owner'],
+        token:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjVmNzNhZjIwNDk1NTg3MmRmNDc4MzQzNiIsIm5hbWUiOiJHdXN0YXZvIiwiZW1haWwiOiJndXN0YXZvc29obmUzOEBnbWFpbC5jb20iLCJwYXNzd29yZCI6IjgxNzQzOWM5YzY1MzI5ODhlYTlhMTMyZDExYjY2ZjNlIiwiX192IjowfSwiaWF0IjoxNjAxNTAxMTEzLCJleHAiOjE2MDE1ODc1MTN9.FPOqvLuo76ZanvRAZJJbxNhnaalYY7f7aDEKhT4Yzkw',
+      })
     expect(response.status).toBe(201)
   })
 })
